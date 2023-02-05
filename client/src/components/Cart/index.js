9fae6e023b17b6dc8f30a3928fdbd82de9bf6871
Jatin_Saini <sainijatin247@gmail.com>
2023-02-05 15:36:28 -0500

    stripe checkout redirect to frontend

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -6,7 +6,19 @@ import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART } from '../../utils/actions';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyQuery } from '@apollo/client';
 
+const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+useEffect(() => {
+  if (data) {
+    stripePromise.then((res) => {
+      res.redirectToCheckout({ sessionId: data.checkout.session });
+    });
+  }
+}, [data]);
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
   useEffect(() => {
@@ -38,6 +50,18 @@ const Cart = () => {
     });
     return sum.toFixed(2);
   }
+  function submitCheckout() {
+    getCheckout({
+      variables: { products: productIds }
+    });
+    const productIds = [];
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+  }
   return (
 <div className="cart">
   <div className="close" onClick={toggleCart}>[close]</div>
@@ -51,7 +75,7 @@ const Cart = () => {
         <strong>Total: ${calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
+            <button onClick={submitCheckout} >
               Checkout
             </button>
             :
